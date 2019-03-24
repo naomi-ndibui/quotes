7a06e5b4525afb255866c567898283f3c0699f14
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-24 14:11:34 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
